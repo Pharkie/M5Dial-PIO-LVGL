3e568c4ef5fc86b7e699977571d662a6e7dd39ee
Adam Knowles <1413836+Pharkie@users.noreply.github.com>
2025-09-30 13:10:37 +0100

    works

--- examples/hello_world/src/hello_world.cpp
+++ examples/hello_world/src/hello_world.cpp
@@ -4,14 +4,18 @@
 #include <M5Unified.hpp>
 #include <freertos/FreeRTOS.h>
 #include <lvgl.h>
+#include <esp_log.h>
+
 #include "M5Dial-LVGL.h"
+
+static const char *TAG = "hello_world";
 
 void hello_world(void)
 {
     lv_obj_set_style_bg_color(lv_screen_active(), lv_color_white(), LV_PART_MAIN);
 
     lv_obj_t *label = lv_label_create(lv_screen_active());
-    lv_label_set_text(label, "Hello M5Dial");
+    lv_label_set_text(label, "Hello Greta");
     lv_obj_set_style_text_color(lv_screen_active(), lv_color_black(), LV_PART_MAIN);
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 }
@@ -32,6 +36,7 @@ extern "C"
 {
     void app_main()
     {
+        ESP_LOGI(TAG, "Program starting");
         xTaskCreatePinnedToCore(main_task, "main_task", 8192, nullptr, 1, nullptr, 1);
     }
 }
